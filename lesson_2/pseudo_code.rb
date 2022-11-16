40e504f481c83500cf0b6b9e842a64ab71ea8552
Mordecai Coleman <mcolemangit@gmail.com>
2022-11-15 22:30:04 -0500

    half of pseud assignment

---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,66 @@
+# 1) Method that returns the sum of two integers
+
+# Casual pseudo-code
+
+# Ask user to enter one integer, then a second.
+# Perform addition operation
+# Return result
+
+# Formal pseudo-code
+
+# START
+
+# GET number from user
+# SET number1 = user input
+# GET second number from user
+# SET number2 = user input
+# SET result = number1 + number2, converting to integers
+# Return result
+
+# END
+
+# 2) Method that takes an array of string, and returns a string that is all those
+# strings concatenated together
+
+# Casual 
+
+# Given an array of strings
+# Concatenate each string to form one string
+# Print conatenated string
+
+# Formal
+
+# START
+
+# Given array of strings
+# SET new empty string
+# ITERATE through array , adding each element to new empty string
+# RETURN new string
+
+# END
+
+# 3) a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element.
+
+# Casual
+
+# Given an array of integers
+# Make new empty array
+# Iterate over given array, returning every other integer to new array
+
+# Formal
+
+# START
+# Given array of integers
+# SET new empty array
+# ITERATE over given array using .each_with_index
+# ADD every other element to new array
+# RETURN new array
+
+# END
+
+# 4) a method that determines the index of the 3rd occurrence of a given character in a string.
+
+# Casual
+
+# Given string
+# 
